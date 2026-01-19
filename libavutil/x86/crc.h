c2eae0186b77990be9d6daf170e2e05daae7062a
Andarwinux <Andarwinux@users.noreply.github.com>
2026-01-19 00:00:00 +0000

    replace ff_reverse with __builtin_elementwise_bitreverse
    lowered as a single instruction on x86 GFNI or aarch64

--- libavutil/x86/crc.h
+++ libavutil/x86/crc.h
@@ -106,15 +106,7 @@ static const AVCRC crc_table_clmul[AV_CRC_MAX][17] = {
 
 static uint64_t reverse(uint64_t p, unsigned int deg)
 {
-    uint64_t ret = 0;
-    int i;
-    for (i = 0; i < (deg / 8); i += 1) {
-        ret = (ret << 8) | (ff_reverse[p & 0xff]);
-        p >>= 8;
-    }
-    int rem = (deg + 1) - 8 * i;
-    ret = (ret << rem) | (ff_reverse[p & 0xff] >> (8 - rem));
-    return ret;
+    return __builtin_elementwise_bitreverse(p) >> (64 - deg);
 }
 
 static uint64_t xnmodp(unsigned n, uint64_t poly, unsigned deg, uint64_t *div, int bitreverse)
